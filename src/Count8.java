d8c8cc8222cdf68b6180ac0046b891bd81e8bf45
Akshay Kumar <aksh0001@student.monash.edu>
2019-11-03 14:16:01 -0600

    added count 8

--- src/Count8.java
+++ src/Count8.java
@@ -1,3 +1,10 @@
+/*
+Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit,
+except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4.
+count8(8) → 1
+count8(818) → 2
+count8(8818) → 4
+ */
 public class Count8 {
     public static void main(String[] args) {
         System.out.println(count8(8));
