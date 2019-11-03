ccbcb25d4ba31a90dbf6b79b15fac1cc2b5c8168
Akshay Kumar <aksh0001@student.monash.edu>
2019-11-03 14:14:25 -0600

    added count 8

---
+++ src/Count8.java
@@ -0,0 +1,25 @@
+public class Count8 {
+    public static void main(String[] args) {
+        System.out.println(count8(8));
+        System.out.println(count8(818));
+        System.out.println(count8(123));
+        System.out.println(count8(8818));
+
+    }
+
+    static int count8(int n) {
+        // BC: if n is 0, no 8s exist
+        if (n == 0)
+            return 0;
+        if (n % 10 == 8) {
+            // RC1: if two rightmost digits is 88, count as 2, and recurse on rest
+            if (n % 100 == 88)
+                return 2 + count8(n / 10);
+            else
+                // RC2: if rightmost digit is just one 8, standard--count as 1 and recurse on rest
+                return 1 + count8(n / 10);
+        } else
+            // RC3: if rightmost not 8, recurse on rest
+            return count8(n / 10);
+    }
+}
