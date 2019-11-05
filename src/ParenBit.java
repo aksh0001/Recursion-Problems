9896199d86a5e68e0bc42ba19e94f03ffc476107
Akshay Kumar <aksh0001@student.monash.edu>
2019-11-05 11:17:39 -0600

    added Paren bit problem

--- src/ParenBit.java
+++ src/ParenBit.java
@@ -19,14 +19,24 @@ public class ParenBit {
         return parenBit(str, 0, str.length() - 1);
     }
 
+    /**
+     * L-R (Left, Right) Recursive pattern approach.
+     * parenBit(s, i, j) returns the paren bit of s bounded at [i:j].
+     * Therefore, if s[i] = '(' and s[j] = ')' we can return this substring; otherwise recurse left/right appropriately.
+     *
+     * @param s string
+     * @param i left pointer
+     * @param j right pointer
+     * @return the paren bit
+     */
     static String parenBit(String s, int i, int j) {
         //BC1: If empty string/pointers overlap, nothing to return
         if (i > j)
             return "";
-        //RC1: If s[i] is not an open paren '(', then recurse right
+        //RC1: If s[i] is not an open paren '(', then recurse right to exclude this s[i]
         if (s.charAt(i) != '(')
             return parenBit(s, i + 1, j);
-        //RC2: If s[j] is not a closed paren ')', then recurse left
+        //RC2: If s[j] is not a closed paren ')', then recurse left to exclude this s[j]
         if (s.charAt(j) != ')')
             return parenBit(s, i, j - 1);
         else    //Else, pointers i and j are at the open and closed paren, return this substring
